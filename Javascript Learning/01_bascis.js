3eca96ffeed0ff6ddfe3e2210327fe020e494667
SHWETA WADHWA <125170830+shwetawadhwa26@users.noreply.github.com>
2024-06-25 11:37:55 +0530

    Add files via upload

---
+++ Javascript Learning/01_bascis.js
@@ -0,0 +1,36 @@
+//  console.log - used to print a message to the console
+
+console.log("apna college");
+console.log("hello");
+
+name='shweta'
+console.log(name);
+
+price=999
+console.log(price);
+
+x=null
+console.log(x);
+
+y=undefined
+console.log(y); 
+
+
+const account_id=14453
+let acountemail="shwetawadhwa75@gmail.com"
+var accountpassword="12345"
+accountcity="jaipur"
+
+let accountstate;
+// account_id=2345
+console.log(account_id);
+
+accountemail="hc@hc.com"
+accountpassword="12343456"
+accountcity="panchkula"
+
+console.table([account_id,accountemail,accountpassword,accountpassword,accountcity,accountstate])
+
+
+
+//prefer not to use var coz issues in block scope and functional scope
