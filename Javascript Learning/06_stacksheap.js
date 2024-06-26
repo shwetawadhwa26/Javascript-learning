a3f4a93163b64cd821c2874fba8ad26d2c7975ef
SHWETA WADHWA <125170830+shwetawadhwa26@users.noreply.github.com>
2024-06-26 14:14:29 +0530

    Add files via upload

---
+++ Javascript Learning/06_stacksheap.js
@@ -0,0 +1,18 @@
+//MEMORY TYPES - STACK(primitive) AND HEAP(non-primitive)
+
+let myyoutubename = "chaiaurcode"
+let anothername =myyoutubename
+anothername="histeshchoudharydotcom"
+console.log(anothername);
+
+let userone={
+    email:"shwet2003@gmail.com",
+    upi:"user@ybl"
+}
+
+let usertwo=userone
+
+usertwo.email="hitesh@google.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
