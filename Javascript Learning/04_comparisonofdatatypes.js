278685fdacbc6d5e7bdc0d14a2377d3a2614a7b5
SHWETA WADHWA <125170830+shwetawadhwa26@users.noreply.github.com>
2024-06-26 11:08:10 +0530

    Add files via upload

---
+++ Javascript Learning/04_comparisonofdatatypes.js
@@ -0,0 +1,30 @@
+console.log(2>1);
+console.log(2>+1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+
+
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined == 0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+
+//clean code is more valuable try not to make such comparisons btw null and undefined 
+
+
+console.log("2" ==2);
+//true output as string is converted to number
+console.log("2" === 2);
+// === equal to & type. output will be false as in === there is no conversion from string to number
+
